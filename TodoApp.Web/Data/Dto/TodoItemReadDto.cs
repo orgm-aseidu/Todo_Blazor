53bd3606763b435c19232adc5b3aef9a3bdc70af
orgm-aseidu <ayishetu.seidu@orgmento.com>
2022-10-10 10:28:25 +0000

    created todo item DTOs

---
+++ TodoApp.Web/Data/Dto/TodoItemReadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp.Web.Data.Dto
+{
+    public class TodoItemReadDto
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public bool IsCompleted { get; set; } = false;
+    }
+}
