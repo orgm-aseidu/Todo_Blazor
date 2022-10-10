e00e047d4fb3976052bdebc4e60c17851257b70c
orgm-aseidu <ayishetu.seidu@orgmento.com>
2022-10-10 11:02:17 +0000

    created base class for todo items

---
+++ TodoApp.Web/Pages/Bases/TodosBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using TodoApp.Web.Data.Dto;
+using TodoApp.Web.Models;
+
+namespace TodoApp.Web.Pages.Bases
+{
+    public class TodosBase : ComponentBase
+    {
+        public List<TodoItem> Todos { get; set; }
+
+        public void AddItem (TodoItemCreateDto item)
+        {
+             Todos.Add(new TodoItem() { Task=item.Task});
+        }
+
+        public void RemoveItem(TodoItem item)
+        {
+            Todos.Remove(item);
+        }
+    }
+}
