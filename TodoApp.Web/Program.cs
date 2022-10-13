6898be5ccfd2d1721f06eed41556691680ad0ce9
orgm-aseidu <ayishetu.seidu@orgmento.com>
2022-10-13 12:15:25 +0000

    installed and registered blazor local storage

--- TodoApp.Web/Program.cs
+++ TodoApp.Web/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TodoApp.Web;
@@ -6,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
